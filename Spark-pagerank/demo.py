8a057f50c9b8b3dd8a10a72ba0f7289b70fb290b
muyuuuu <muyu0223@outlook.com>
2021-03-24 21:41:12 +0000

    spark pagerank finish :art:

--- Spark-pagerank/demo.py
+++ Spark-pagerank/demo.py
@@ -1,24 +1,77 @@
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
-from pyspark.mllib.linalg import Vectors, SparseVector, DenseVector
+import re
+import sys
 from operator import add
+from pyspark import SparkConf, SparkContext
+
+def compute_contribs(urls, rank):
+    """
+    给urls计算
+    Args:
+        urls: 目标url相邻的urls集合
+        rank: 目标url的当前rank
+
+    Returns:
+        url: 相邻urls中的一个url
+        rank: 当前url的新的rank
+    """
+    num_urls = len(urls)
+    for url in urls:
+        yield (url, rank / num_urls)
 
 
-# master 为当前节点，count app 是引用的名称
-sc = SparkContext("local", "count app")
-# 取消日志的输出
-# sc.addPyFile("Vertex.py")
-SparkSession.builder.getOrCreate().sparkContext.setLogLevel("WARN")
-tmp = 0
-def update(x):
-    global tmp
-    # x += tmp
-    tmp += 1
-    return (x, tmp)
+def split_url(url_line):
+    """
+    把一行url切分开来
+    Args:
+        url_line: 一行url，如 1 2
 
-ls = [1, 1, 1, 1, 1]
-ls_rdd = sc.parallelize(ls)
+    Returns:
+        url, neighbor_url
+    """
+    parts = re.split(r'\s+', url_line) # 正则
+    return parts[0], parts[1]
 
-ls_map = ls_rdd.map(lambda x: update(x))
-ls_data = ls_map.collect()
-print(ls_data)
+
+def compute_pagerank(sc, url_data_file, iterations):
+    """
+    计算各个page的排名
+    Args:
+        sc: SparkContext
+        url_data_file: 测试数据文件
+        iterations: 迭代次数
+
+    Returns:
+        status: 成功就返回0
+    """
+
+    # 读取url文件 ['1 2', '1 3', '2 1', '3 1']
+    lines = sc.textFile(url_data_file).map(lambda line: line.encode('utf8'))
+
+    # 建立Pair RDD (url, neighbor_urls) [(1,[2,3]), (2,[1]), (3, [1])]
+    links = lines.map(lambda line : split_url(line)).distinct().groupByKey().mapValues(lambda x: list(x)).cache()
+    # 初始化所有url的rank为1 [(1, 1), (2, 1), (3, 1)]
+    ranks = lines.map(lambda line : (line[0], 1))
+
+    for i in range(iterations):
+        # (url, [(neighbor_urls), rank]) join neighbor_urls and rank
+        # 把当前url的rank分别contribute到其他相邻的url (url, rank)
+        contribs = links.join(ranks).flatMap(
+            lambda url_urls_rank: compute_contribs(url_urls_rank[1][0], url_urls_rank[1][1])
+            )
+        # 把url的所有rank加起来，再赋值新的
+        ranks = contribs.reduceByKey(add).mapValues(lambda rank : rank * 0.85 + 0.15)
+
+    for (link, rank) in ranks.collect():
+        print("%s has rank %s." % (link, rank))
+
+    return 0
+
+if __name__ == '__main__':
+    data_file = "data.txt"
+    iterations = 10
+
+    conf = SparkConf().setAppName('PythonPageRank')
+    sc = SparkContext(conf=conf)
+
+    ret = compute_pagerank(sc, data_file, iterations)
+    sys.exit(ret)
--- Spark-pagerank/main.py
+++ Spark-pagerank/main.py
@@ -1,52 +1,98 @@
+from pyspark import SparkContext, SparkConf
+import time
 from operator import add
-import numpy as np
-from pyspark import SparkConf, SparkContext
 
 
-# 配置文件
+# 节点到数字，数字到节点
+name_to_id, id_to_name = {}, {}
+# 当前节点指向了哪些边
+point_edges = []
+# 节点数量, 边的数量
+vertex_num, edge_num = 0, 0
+# 迭代次数
+max_iter_num = 15
+# 跳转概率
+alpha = 0.85
+
+# spark 配置
 conf = SparkConf().setMaster("local").setAppName("PageRank")
 sc = SparkContext(conf=conf)
 
-# 加载文件为 RDD
-# links_data_rdd = sc.textFile("link.data")
-
-# 获取最初的概率转移矩阵
-a = np.zeros((10, 10))
-for num in range(60):
-    for row, col in zip(np.random.randint(0, 10, 1), np.random.randint(0, 10, 1)):
-        a[row, col] = 1.0
-
-# 初始化分数
-pagerank = sc.parallelize([1 / 10 for i in range(10)])
-
-# 修正节点没有出度的问题
-revise = np.zeros((10, 10))
-zero = np.sum(a, axis=0)
-for i in zero:
-    if i == 0:
-        revise[:, i] = 1 / 10
-
-a += revise
-
-# 一个页面指向两个页面，那么分数要平分
-a = a / np.sum(a, axis=0)
-a = sc.parallelize(a)
-
-alpha = 0.85
-jump_value = 0.15 / 10
-difference = 1e-4
-max_num = 100
-tmp_pagerank = 0
-
-for i in range(max_num):
-    # 减少计算量
-    if i % 10 == 0:
-        tmp = pagerank.reduce(add)
-        if np.sum(tmp - tmp_pagerank) < difference:
-            break
-        else:
-            tmp_pagerank = tmp
-    a = a.map(lambda x: x * pagerank)
+def get_vtx_id(name, isOut=False):
+    '''
+    将节点的名称映射为数字
+    '''
+    global vertex_num
+    if name not in name_to_id:
+        # 字典，key 是节点名，值是节点的编号
+        name_to_id[name] = vertex_num
+        # 编号到节点
+        id_to_name[vertex_num] = name
+        vertex_num += 1
+        # 追加一个空列表，用于存储自己指向哪些边
+        point_edges.append([])
+    return name_to_id[name]
 
 
-print(a.collect())
+def add_edge(source_id, target_id):
+    '''
+    source_id 指向了 target_id
+    '''
+    point_edges[source_id].append(target_id)
+
+
+def get_data(file_name):
+    global edge_num
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            nodes = line.split()
+            # 目标节点
+            target = nodes[0]
+            target_id = get_vtx_id(target)
+            # 这些节点指向了目标节点
+            for source in nodes[1:]:
+                source_id = get_vtx_id(source, isOut=True)
+                edge_num += 1
+                add_edge(source_id, target_id)
+
+
+def compute_contribs(x):
+    '''
+    当前节点，以及分数
+    '''
+    urls, rank = x[1][0], x[1][1]
+    num_urls = len(urls)
+    for url in urls:
+        yield (url, rank / num_urls)
+
+
+if __name__ == "__main__":
+    
+    get_data("link.data")
+    print("Loaded data")
+
+    print("Vertex number is {}, Edge number is {}".format(vertex_num, edge_num))
+    assert vertex_num == len(point_edges)
+
+    # 临时变量，便于映射
+    nodes = [i for i in range(vertex_num)]
+    # 生成 RDD，RDD的结构是 <id, [urls]>, id 是节点编号，urls 是自己指向了哪些节点，一个列表
+    links = sc.parallelize(nodes).map(lambda x: (x, point_edges[x])).cache()
+    # 初始化 rank 值
+    ranks = sc.parallelize(nodes).map(lambda x: (x, 1/vertex_num)).cache()
+    print("Initialize RDD of links and ranks")
+
+    jump_value = (1 - alpha) / vertex_num
+
+    since = time.time()
+    for i in range(max_iter_num):
+        # join 把节点编号相同的放在一起，[urls, rank]
+        # flatMap 计算每个节点所带有的分数，返回 <url_id, rank>
+        contribs = links.join(ranks).flatMap(lambda x: compute_contribs(x))
+        # 将 url_id 相同的聚合在一起，也就是被指向的节点分数求和，并更新 rank 值
+        ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * alpha + jump_value)
+    print(time.time() - since)
+    for (link, rank) in ranks.sortBy(lambda x: x[1], False).take(5):
+        print("{}, {}".format(id_to_name[link], rank))
+    print(time.time() - since)
